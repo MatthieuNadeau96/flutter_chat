ceaec696fd656871763cf3a03dca8ba99b35352f
Matthieu <matthieunadeau96@gmail.com>
2020-01-27 15:48:05 -0500

    added styling to the read and unread messages

--- lib/widgets/recent_chats.dart
+++ lib/widgets/recent_chats.dart
@@ -23,52 +23,62 @@ class RecentChats extends StatelessWidget {
             itemCount: chats.length,
             itemBuilder: (BuildContext context, int index) {
               final Message chat = chats[index];
-              return Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Row(
-                    children: <Widget>[
-                      CircleAvatar(
-                        radius: 35.0,
-                        backgroundImage: AssetImage(chat.sender.imageUrl),
-                      ),
-                      SizedBox(
-                        width: 10.0,
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(
-                            chat.sender.name,
-                            style: TextStyle(
-                              color: Colors.grey,
-                              fontSize: 15.0,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          Container(
-                            width: MediaQuery.of(context).size.width * 0.45,
-                            child: Text(
-                              chat.text,
+              return Container(
+                margin: EdgeInsets.only(top: 5.0, bottom: 5.0, right: 20.0),
+                padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
+                decoration: BoxDecoration(
+                    color: chat.unread ? Color(0xffffefee) : Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(20.0),
+                      bottomRight: Radius.circular(20.0),
+                    )),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        CircleAvatar(
+                          radius: 35.0,
+                          backgroundImage: AssetImage(chat.sender.imageUrl),
+                        ),
+                        SizedBox(
+                          width: 10.0,
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              chat.sender.name,
                               style: TextStyle(
-                                color: Colors.blueGrey,
+                                color: Colors.grey,
                                 fontSize: 15.0,
-                                fontWeight: FontWeight.w600,
+                                fontWeight: FontWeight.bold,
                               ),
-                              overflow: TextOverflow.ellipsis,
                             ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                  Column(
-                    children: <Widget>[
-                      Text(chat.time),
-                      Text('NEW'),
-                    ],
-                  )
-                ],
+                            Container(
+                              width: MediaQuery.of(context).size.width * 0.45,
+                              child: Text(
+                                chat.text,
+                                style: TextStyle(
+                                  color: Colors.blueGrey,
+                                  fontSize: 15.0,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: <Widget>[
+                        Text(chat.time),
+                        Text('NEW'),
+                      ],
+                    )
+                  ],
+                ),
               );
             },
           ),

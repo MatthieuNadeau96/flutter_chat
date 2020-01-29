aca0ac0b67de048382e13f985f07ddd9c42d29d2
Matthieu <matthieunadeau96@gmail.com>
2020-01-29 16:16:05 -0500

    added heart icon button to the messages list

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -13,7 +13,7 @@ class ChatScreen extends StatefulWidget {
 
 class _ChatScreenState extends State<ChatScreen> {
   _buildMessage(Message message, bool isMe) {
-    return Container(
+    final Container msg = Container(
       margin: isMe
           ? EdgeInsets.only(
               top: 8.0,
@@ -23,9 +23,9 @@ class _ChatScreenState extends State<ChatScreen> {
           : EdgeInsets.only(
               top: 8.0,
               bottom: 8.0,
-              right: 80.0,
             ),
       padding: EdgeInsets.symmetric(horizontal: 25.0, vertical: 15.0),
+      width: MediaQuery.of(context).size.width * 0.75,
       decoration: BoxDecoration(
         color: isMe ? Theme.of(context).accentColor : Color(0XFFFFEFEE),
         borderRadius: isMe
@@ -62,6 +62,24 @@ class _ChatScreenState extends State<ChatScreen> {
           ),
         ],
       ),
+    );
+    if (isMe) {
+      return msg;
+    }
+    return Row(
+      children: <Widget>[
+        msg,
+        IconButton(
+          icon: message.isLiked
+              ? Icon(Icons.favorite)
+              : Icon(Icons.favorite_border),
+          iconSize: 30.0,
+          color: message.isLiked
+              ? Theme.of(context).primaryColor
+              : Colors.blueGrey,
+          onPressed: () {},
+        ),
+      ],
     );
   }
 
